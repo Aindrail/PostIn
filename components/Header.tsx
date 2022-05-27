4d6624abeb822d69459e1fbf030767d381c61a15
Aindrail <aindrailsantra@gmail.com>
2022-05-27 19:56:12 +0530

    Header started and new icon loaded

---
+++ components/Header.tsx
@@ -0,0 +1,18 @@
+import Image from 'next/image'
+import React from 'react'
+
+function Header() {
+  return (
+      <div>
+<div className="relative h-10 w-20"> 
+        <Image objectFit='contain' src="https://play-lh.googleusercontent.com/ItxDn_WNq3QPvuXS1jaXwujqvRobrH-hpEqbDy1iCVx0b5Z5dv9ADyfd83Dn6cDsNbE" layout="fill" />
+        
+
+    </div>
+
+      </div>
+    
+  )
+}
+
+export default Header
--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  images: {
+    domains: ["play-lh.googleusercontent.com"],
+  },
 }
