c24f8f5682679ef4e55f4f8fb6ba281f59b50bfa
Aindrail <aindrailsantra@gmail.com>
2022-06-04 22:58:52 +0530

    Backend created using stepzen and supabase

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,18 +2,24 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import {SessionProvider} from 'next-auth/react'
 import Header from '../components/Header'
+import { ApolloProvider } from '@apollo/client'
+import client from '../apollo-client'
 
 function MyApp({ Component, pageProps: {session, ...pageProps} }: AppProps) {
   return (
+    <ApolloProvider client={client}>
+
+    
     <SessionProvider session={session} refetchInterval={5*60}>
       <div className='h-screen overflow-y-scroll bg-slate-200'>
         <Header />
+        {/* every single page will have the header */}
         <Component {...pageProps} />
       </div>
        
 
     </SessionProvider>
-  
+    </ApolloProvider>
   )
 }
 // use us to use hooks all over
