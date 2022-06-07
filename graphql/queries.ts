e73938ac9cd61ffcc5a1ce81f957f692b0579833
Aindrail <aindrailsantra@gmail.com>
2022-06-07 23:37:23 +0530

    The backend of feed like comment likes and subreddit was done using @materializer of Stepzen

--- graphql/queries.ts
+++ graphql/queries.ts
@@ -1,11 +1,44 @@
 import { gql } from '@apollo/client'
 
-export const GET_SUBREDDITS_BY_TOPIC = gql`
-    query MyQuery($topic: String!) {
-        getSubredditListByTopic(topic: $topic) {
-            id
-            topic
-            created_at
-        }
+export const GET_ALL_POSTS = gql`
+  query MyQuery {
+    getPostList {
+      body
+      id
+      image
+      created_at
+      title
+      subreddit_id
+      username
+      subreddit {
+        created_at
+        id
+        topic
+      }
+      comments {
+        text
+        username
+        created_at
+        id
+        post_id
+      }
+      votes {
+        created_at
+        id
+        post_id
+        upvote
+        username
+      }
     }
+  }
+`
+
+export const GET_SUBREDDITS_BY_TOPIC = gql`
+  query MyQuery($topic: String!) {
+    getSubredditListByTopic(topic: $topic) {
+      id
+      topic
+      created_at
+    }
+  }
 `
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,26 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import Feed from '../components/Feed'
 import Header from '../components/Header'
 import PostBox from '../components/PostBox'
 
 const Home: NextPage = () => {
   return (
-    <div className="max-w-5xl my-7 mx-auto">
+    <div className="my-7 mx-auto max-w-5xl">
       <Head>
         <title>PostIn</title>
         {/* <link rel="icon" href="/favicon.ico" /> */}
       </Head>
 
-       <PostBox />
+      <PostBox />
 
- 
+      <div className="flex justify-center">
+        <Feed />
+        </div>
+
+
     </div>
-    
   )
 }
 
