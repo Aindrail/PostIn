d0176ff340e3545d4574b8d1d68df1ee80f68bd5
Aindrail <aindrailsantra@gmail.com>
2022-06-09 17:25:13 +0530

    The upvote and downvote logic was implemented

--- components/Post.tsx
+++ components/Post.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import {
   ArrowDownIcon,
   ArrowUpIcon,
@@ -13,13 +13,66 @@ import Avatar from './Avatar'
 import TimeAgo from 'react-timeago'
 import Link from 'next/link'
 import { Jelly } from '@uiball/loaders'
+import { useSession } from 'next-auth/react'
+import toast from 'react-hot-toast'
+import { useMutation, useQuery } from '@apollo/client'
+import { GET_ALL_VOTES_BY_POST_ID } from '../graphql/queries'
+import { ADD_VOTE } from '../graphql/mutations'
 
 type Props = {
   post: Post
 }
 
 function Post({ post }: Props) {
+  const [vote, setVote] = useState<boolean>()
+const {data: session} = useSession()
 
+const {data,loading,error} = useQuery(GET_ALL_VOTES_BY_POST_ID,{
+  variables:{
+    post_id:post?.id
+  }
+    })
+    
+
+    const [addVote] = useMutation(ADD_VOTE,{
+      refetchQueries:[ GET_ALL_VOTES_BY_POST_ID,'getVotesByPostId'],
+    })
+
+  const upVote = async (isUpVote: boolean) => {
+    if(!session) {
+      toast("! Sign In to Vote! ")
+      return
+    }
+    if(vote && isUpVote) return;
+    if(vote === false && !isUpVote) return;
+    console.log('voting....',isUpVote);
+    await addVote({
+      variables:{
+        post_id:post.id,
+        username:session.user?.name,
+        upvote: isUpVote,
+      },
+    })
+    
+  }
+  const displayVote = (data: any) => {
+    const votes: Vote[] = data?.getVotesByPostId
+    const displayNumber = votes?.reduce(
+      (total,vote) => (vote.upvote ? (total + 1) : (total - 1)),
+      0
+    )
+    if(votes?.length === 0) return 0;
+    if(displayNumber === 0){
+       return votes[0]?.upvote ? 1 : -1;
+    } 
+    return displayNumber;
+  }
+
+useEffect(()=>{
+  const votes: Vote[]=data?.getVotesByPostId;
+  const vote = votes?.find(vote=>vote.username===session?.user?.name)?.upvote
+  setVote(vote)
+},[data])
 
 if(!post)
 return(
@@ -29,15 +82,18 @@ return(
 )
 
   return (
+    
     <Link href={`/post/${post.id}`}>
-   
     <div className="flex cursor-pointer rounded-md border-gray-300 bg-white shadow-sm hover:border hover:border-gray-600">
       {/* Vote */}
       <div className="flex flex-col items-center justify-start space-y-1 rounded-l-md bg-gray-50 p-4 text-gray-400">
-
-        <ArrowUpIcon className="voteButtons hover:text-red-400" />
-        <p className="text-xs font-bold text-black">0</p>
-        <ArrowDownIcon className="voteButtons hover:text-blue-400" />
+      <Link href='/'>
+        <ArrowUpIcon onClick={() => upVote(true)} className={`voteButtons hover:text-blue-400 ${vote && 'text-blue-400'}`} />
+      </Link>
+        <p className="text-xs font-bold text-black">{displayVote(data)}</p>
+        <Link href='/'>
+        <ArrowDownIcon onClick={() => upVote(false)} className={`voteButtons hover:text-red-400 ${vote===false && 'text-red-400'}`} />
+        </Link>
       </div>
       <div className="p-3 pb-1">
         {/* header of post */}
@@ -55,7 +111,7 @@ return(
                 
             </p>
         </div>
-
+       
         {/* Body of post */}
         <div className='py-4'>
           <h2 className='text-xl font-semibold'>{post.title}</h2>
@@ -93,11 +149,13 @@ return(
             
 
           </div>
-
+         
         </div>
+        
       </div>
+     
     </div>
-    </Link>
+   </Link>
   )
 }
 
